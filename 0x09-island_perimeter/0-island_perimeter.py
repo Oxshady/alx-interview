84d428d5e813c0d9110728d598d09e1f3dc81713
Oxshady <shadymahmod55@gmail.com>
2024-12-05 21:37:18 +0200

    feat: implement island perimeter calculation
    Added a function  to calculate the perimeter of an island represented in a grid.
    Includes handling for connected cells to adjust the perimeter calculation.

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in grid
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 1
+                if row < rows - 1 and grid[row + 1][col] == 1:
+                    perimeter -= 1
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 1
+                if col < cols - 1 and grid[row][col + 1] == 1:
+                    perimeter -= 1
+    return perimeter
